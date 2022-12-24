ead2d4fd1c6cc4743062018755f80f38229f6a9e
anshul45 <80778090+anshul45@users.noreply.github.com>
2022-12-24 23:25:27 +0530

    Created Login && Registered Page

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,6 +1,33 @@
-import { Box } from "@mui/material";
-
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("min-width:1000px");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        text-align="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Social_Media
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" varient="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Social-Media, the Social Media Website!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 };
 export default LoginPage;
