e9a92f8e11f14b0e7d9298fdfb43e3656d20453a
anshul45 <80778090+anshul45@users.noreply.github.com>
2022-12-25 09:28:47 +0530

    Added Homepage

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,6 +1,9 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
   return (
     <Box>
       <Navbar />
